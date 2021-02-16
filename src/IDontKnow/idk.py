e1bc7a3c4b3d24e61a9cd13ccf7359ed2b44eab4
Brian Lucero <13rianlucero@csu.fullerton.edu>
2021-02-16 01:30:56 -0800

    Create idk.py

---
+++ src/IDontKnow/idk.py
@@ -0,0 +1,15 @@
+## ===========================================================================================================================
+## FILE: idk.py
+##  DESCRIPTION: main driver file for the IDK food app
+##  DEVELOPERS: 
+##    - Matthew Padden
+##    - Tommy Nguyen
+##    - Berkeley C. 
+##    - Brian Lucero
+##    - Bilal El-Haghassan
+##
+## RUN THIS FILE: python3 idk.py
+## LANGUAGE: Python 3.9.1 --- 64-bit
+## ===========================================================================================================================
+
+print("What do you wanna eat?")
