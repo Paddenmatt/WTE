88b843baa7ff8031ee6f97a9a0fb48b581620c69
Brian Lucero <13rianlucero@csu.fullerton.edu>
2021-02-16 06:00:11 -0800

    Update idk.py

--- src/IDontKnow/idk.py
+++ src/IDontKnow/idk.py
@@ -1,15 +1,32 @@
 ## ===========================================================================================================================
 ## FILE: idk.py
-##  DESCRIPTION: main driver file for the IDK food app
-##  DEVELOPERS: 
-##    - Matthew Padden
-##    - Tommy Nguyen
-##    - Berkeley C. 
-##    - Brian Lucero
-##    - Bilal El-Haghassan
+##    DESCRIPTION: main driver file for the IDK food app
 ##
 ## RUN THIS FILE: python3 idk.py
-## LANGUAGE: Python 3.9.1 --- 64-bit
+##    LANGUAGE: Python 3.9.1 --- 64-bit
+##
+## Copyright (C) 2021  Brian Lucero, Berkeley C., Bilal El-Haghassan, Matthew Padden, Tommy Nguyen
+##
+##    This program is free software: you can redistribute it and/or modify
+##    it under the terms of the GNU Affero General Public License as published
+##    by the Free Software Foundation, either version 3 of the License, or
+##    (at your option) any later version.
+##
+##    This program is distributed in the hope that it will be useful,
+##    but WITHOUT ANY WARRANTY; without even the implied warranty of
+##    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##    GNU Affero General Public License for more details.
+##
+##    You should have received a copy of the GNU Affero General Public License
+##    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+##
+## DEVELOPERS:            EMAILS:
+##    Matthew Padden ------- paddenmatt@csu.fullerton.edu
+##    Tommy Nguyen --------- nguyentommy66@csu.fullerton.edu
+##    Berkeley C. ---------- berkeley@csu.fullerton.edu
+##    Brian Lucero --------- 13rianlucero@csu.fullerton.edu
+##    Bilal El-Haghassan --- bilalelhaghassan@csu.fullerton.edu
+##
 ## ===========================================================================================================================
 
 print("What do you wanna eat?")
