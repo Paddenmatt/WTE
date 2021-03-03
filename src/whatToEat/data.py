d71030ac502d11cdc46219445998095025a9ff3f
belhaghassan <51680128+belhaghassan@users.noreply.github.com>
2021-03-03 12:34:29 -0800

    Updated database to dict's

---
+++ src/whatToEat/data.py
@@ -0,0 +1,90 @@
+
+foodlist = {
+            Fries, BakedSalmon, SteakIn, SteakOut, GrilledChicken, 
+            GrilledSalmon, Ravioli, Burrito, Spaghetti, Omelet, 
+            Nachos, Hamburger, IceCream, HotDog
+            }
+
+Fries = {
+          "dining" : "dining out",
+          "health" : "casual",
+          "flavor1" : "salty",
+          "weight" : "light"
+        } 
+BakedSalmon = {
+          "dining" : "dining in",
+          "health" : "healthy",
+          "flavor1" : "salty",
+          "weight" : "full"
+        }
+SteakIn = {
+          "dining" : "dining in",
+          "health" : "healthy",
+          "flavor1" : "salty",
+          "weight" : "full"
+        }
+SteakOut = {
+          "dining" : "dining out",
+          "health" : "Healthy",
+          "flavor1" : "salty",
+          "weight" : "full"
+        } 
+GrilledChicken = {
+          "dining" : "dining out",
+          "health" : "Healthy",
+          "flavor1" : "salty",
+          "weight" : "light"
+        }  
+GrilledSalmon = {
+          "dining" : "dining out",
+          "health" : "healthy",
+          "flavor1" : "salty",
+          "weight" : "light"
+        }
+Ravioli = {
+          "dining" : "dining in",
+          "health" : "casual",
+          "flavor1" : "salty",
+          "weight" : "full"
+        } 
+Burrito = {
+          "dining" : "dining out",
+          "health" : "casual",
+          "flavor1" : "salty",
+          "weight" : "full"
+        } 
+Spaghetti = {
+          "dining" : "dining out",
+          "health" : "casual",
+          "flavor1" : "salty",
+        }
+Omelet = {
+          "dining" : "dining out",
+          "health" : "casual",
+          "flavor1" : "salty",
+          "weight" : "full"
+        }
+Nachos = {
+          "dining" : "dining out",
+          "health" : "casual",
+          "flavor1" : "salty",
+          "weight" : "full"
+        }
+Hamburger = {
+          "dining" : "dining out",
+          "health" : "casual",
+          "flavor1" : "salty",
+          "weight" : "full"
+        } 
+IceCream = {
+          "dining" : "dining out",
+          "health" : "casual",
+          "flavor1" : "sweet",
+          "weight" : "light"
+        } 
+HotDog = {
+          "dining" : "dining out",
+          "health" : "casual",
+          "flavor1" : "salty",
+          "weight" : "light"
+        } 
