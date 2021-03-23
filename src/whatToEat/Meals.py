8113e3681dcdfdd34c14cd192adfb59162665af0
belhaghassan <51680128+belhaghassan@users.noreply.github.com>
2021-03-23 15:23:13 -0700

    Update Meals.py

--- src/whatToEat/Meals.py
+++ src/whatToEat/Meals.py
@@ -1,4 +1,4 @@
-class Fooditem(object):
+class Fooditem(object): 
     def __init__(self, itemName, mealType, dining, flavor1, health, weight, attribute=0):
         self.itemName = itemName
         self.mealType = mealType
@@ -11,25 +11,31 @@ class Fooditem(object):
     def __gt__(self, other):
         return self.attribute < other.attribute
 
+    def getItemName():
+        return self.itemName
 
 
 # Creates an array of Fooditem objects
 foodList = []
 
-foodList.append(Fooditem('Fries','lunch', 'dining out', 'salty', 'casual', 'light'))
-foodList.append(Fooditem('Baked Salmon','dinner', 'dining out', 'salty', 'healthy', 'light'))
-foodList.append(Fooditem('Steak','dinner', 'dining out', 'salty', 'healthy', 'light'))
-foodList.append(Fooditem('Ravioli','dinner', 'dining out', 'salty', 'healthy', 'full'))
-#foodList.append(Fooditem("GrilledChicken", "dining out", "Healthy", "salty", "light")
-#foodList.append(Fooditem("GrilledSalmon", "dining out", "healthy", "salty", "light")
-#foodList.append(Fooditem("Ravioli", "dining in", "casual", "salty", "full")
-#foodList.append(Fooditem("Burrito", "dining out", "casual", "salty", "full")
-#foodList.append(Fooditem("Spaghetti", "dining out", "casual", "salty", "full")
-#foodList.append(Fooditem("Fries", "dining out", "casual", "salty", "light")
-#foodList.append(Fooditem("Fries", "dining out", "casual", "salty", "light")
-#foodList.append(Fooditem("Fries", "dining out", "casual", "salty", "light")
-#foodList.append(Fooditem("Fries", "dining out", "casual", "salty", "light")
-#foodList.append(Fooditem("Fries", "dining out", "casual", "salty", "light")
+foodList.append(Fooditem("Fries", "lunch", "dining out", "salty", "casual", "light"))
+foodList.append(Fooditem("Fries", "dinner", "dining out", "salty", "casual", "light"))
+foodList.append(Fooditem("Baked Salmon", "dinner", "dining out", "salty", "healthy", "light"))
+foodList.append(Fooditem("Steak", "dinner", "dining out", "salty", "healthy", "light"))
+foodList.append(Fooditem("Ravioli", "dinner", "dining out", "salty", "healthy", "full"))
+foodList.append(Fooditem("GrilledChicken", "lunch", "dining out", "Healthy", "salty", "light"))
+foodList.append(Fooditem("GrilledSalmon", "lunch", "dining out", "healthy", "salty", "light"))
+foodList.append(Fooditem("GrilledSalmon", "dinner", "dining out", "healthy", "salty", "light"))
+foodList.append(Fooditem("Ravioli", "dinner", "dining in", "casual", "salty", "full"))
+foodList.append(Fooditem("Burrito", "lunch", "dining out", "casual", "salty", "full"))
+foodList.append(Fooditem("Spaghetti", "lunch", "dining out", "casual", "salty", "full"))
+foodList.append(Fooditem("Spaghetti", "dinner", "dining out", "casual", "salty", "full"))
+foodList.append(Fooditem("Omelet", "breakfast", "dining out", "casual", "salty", "full"))
+foodList.append(Fooditem("Nachos", "lunch", "dining out", "casual", "salty", "full"))
+foodList.append(Fooditem("Hamburger", "lunch", "dining out", "casual", "salty", "full"))
+foodList.append(Fooditem("IceCream", "lunch", "dining out", "casual", "sweet", "light"))
+foodList.append(Fooditem("IceCream", "dinner", "dining out", "casual", "sweet", "light"))
+foodList.append(Fooditem("HotDog","lunch", "dining out", "casual", "salty", "light"))
 
 # foodlist = [
 #             Fooditem(Fries), Fooditem(BakedSalmon), Fooditem(SteakIn), Fooditem(SteakOut), Fooditem(GrilledChicken),
